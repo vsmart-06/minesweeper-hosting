8f6e67b011bc410055c7b645907293ada0b8d904
vsmart-06 <srivishnuvusirikala@gmail.com>
2022-01-15 20:14:29 +0530

    Removed an issue

--- records.py
+++ records.py
@@ -1,23 +1,5 @@
 import psycopg as db
 import os
-db_url = os.getenv("DATABASE_URL")
-conn = db.connect(db_url)
-c = conn.cursor()
-
-try:   
-    c.execute('''CREATE TABLE user_data (
-        user_id BIGINT NOT NULL PRIMARY KEY,
-        best_time INT,
-        games_won INT NOT NULL,
-        games_lost INT NOT NULL,
-        total_games INT NOT NULL,
-        win_percent DECIMAL NOT NULL,
-        tot_time INT,
-        avg_time INT,
-        privacy VARCHAR
-        )''')
-except db.errors.ProgrammingError:
-    pass
 
 def stats_update(id, win):
     db_url = os.getenv("DATABASE_URL")
@@ -133,6 +115,3 @@ def privacy_change(id, p):
         pass
     c.close()
     conn.close()
-
-c.close()
-conn.close()
