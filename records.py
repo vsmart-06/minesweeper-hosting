d7b88ea98af78e50f7b095d21fac1c2fa266d2c9
vsmart-06 <srivishnuvusirikala@gmail.com>
2022-01-15 17:00:50 +0530

    Shifted back to mysql

--- records.py
+++ records.py
@@ -1,10 +1,11 @@
-import psycopg as db
+import mysql.connector as db
 import os
-db_url = os.getenv("DATABASE_URL")
+
+db_url = os.getenv("JAWSDB_URL")
+
 conn = db.connect(db_url)
 c = conn.cursor()
-
-try:   
+try:
     c.execute('''CREATE TABLE user_data (
         user_id BIGINT NOT NULL PRIMARY KEY,
         best_time INT,
@@ -15,7 +16,7 @@ try:
         tot_time INT,
         avg_time INT,
         privacy VARCHAR
-        );''')
+        )''')
 except db.errors.ProgrammingError:
     pass
 
@@ -24,41 +25,41 @@ def stats_update(id, win):
         if win == 1:
             c.execute('''INSERT INTO user_data 
             (user_id, games_won, games_lost, total_games, win_percent) 
-            VALUES ('''+str(id)+", 1, 0, 1, 100);")
+            VALUES ('''+str(id)+", 1, 0, 1, 100)")
         else:
             c.execute('''INSERT INTO user_data 
             (user_id, games_won, games_lost, total_games, win_percent) 
-            VALUES ('''+str(id)+", 0, 1, 1, 0);")
+            VALUES ('''+str(id)+", 0, 1, 1, 0)")
         conn.commit()
     except db.errors.IntegrityError:
-        c.execute("SELECT * FROM user_data WHERE user_id = "+str(id)+";")
+        c.execute("SELECT * FROM user_data WHERE user_id = "+str(id))
         record = c.fetchone()
         if win == 1:
             new_wins = record[2]+1
             new_total = record[4]+1
             new_percent = (new_wins/new_total)*100
-            c.execute("UPDATE user_data SET games_won = "+str(new_wins)+", total_games = "+str(new_total)+", win_percent = "+str(new_percent)+" WHERE user_id = "+str(id)+";")
+            c.execute("UPDATE user_data SET games_won = "+str(new_wins)+", total_games = "+str(new_total)+", win_percent = "+str(new_percent)+" WHERE user_id = "+str(id))
         else:
             new_lost = record[3]+1
             new_total = record[4]+1
             new_percent = (record[2]/new_total)*100
-            c.execute("UPDATE user_data SET games_lost = "+str(new_lost)+", total_games = "+str(new_total)+", win_percent = "+str(new_percent)+" WHERE user_id = "+str(id)+";")
+            c.execute("UPDATE user_data SET games_lost = "+str(new_lost)+", total_games = "+str(new_total)+", win_percent = "+str(new_percent)+" WHERE user_id = "+str(id))
         conn.commit()
 
 def score_check(id, time):
-    c.execute("SELECT * FROM user_data WHERE user_id = "+str(id)+";")
+    c.execute("SELECT * FROM user_data WHERE user_id = "+str(id))
     record = c.fetchone()
     if record[1] == None:
-        c.execute("UPDATE user_data SET best_time = "+str(time)+", tot_time = "+str(time)+", avg_time = "+str(time)+" WHERE user_id = "+str(id)+";")
+        c.execute("UPDATE user_data SET best_time = "+str(time)+", tot_time = "+str(time)+", avg_time = "+str(time)+" WHERE user_id = "+str(id))
         conn.commit()
         return "new high"
     else:
         old_tot_time = record[6]
         new_tot_time = old_tot_time+time
         new_avg_time = int(new_tot_time/record[2])
-        c.execute("UPDATE user_data SET tot_time = "+str(new_tot_time)+", avg_time = "+str(new_avg_time)+" WHERE user_id = "+str(id)+";")
+        c.execute("UPDATE user_data SET tot_time = "+str(new_tot_time)+", avg_time = "+str(new_avg_time)+" WHERE user_id = "+str(id))
         if time < record[1]:
-            c.execute("UPDATE user_data SET best_time = "+str(time)+" WHERE user_id = "+str(id)+";")
+            c.execute("UPDATE user_data SET best_time = "+str(time)+" WHERE user_id = "+str(id))
             conn.commit()
             return "new record"
     conn.commit()
@@ -69,7 +70,7 @@ def global_leaderboard():
     c.execute('''
                 SELECT user_id, best_time
                 FROM user_data
-                ORDER BY best_time;''')
+                ORDER BY best_time''')
     leaders = c.fetchmany(10)
     return leaders
 
@@ -77,7 +78,7 @@ def server_leaderboard(members):
     server_leaders = []
     for x in members:
         try:
-            c.execute("SELECT user_id, best_time FROM user_data WHERE user_id = "+str(x)+";")
+            c.execute("SELECT user_id, best_time FROM user_data WHERE user_id = "+str(x))
             server_leaders.append(c.fetchone())
         except db.errors.OperationalError:
             pass
@@ -91,14 +92,14 @@ def server_leaderboard(members):
 
 def profile(id):
     try:
-        c.execute("SELECT * FROM user_data WHERE user_id = "+str(id)+";")
+        c.execute("SELECT * FROM user_data WHERE user_id = "+str(id))
         return c.fetchone()
     except db.errors.OperationalError:
         pass
 
 def privacy_change(id, p):
     try:
-        c.execute("UPDATE user_data SET privacy = '"+str(p)+"' WHERE user_id = "+str(id)+";")
+        c.execute("UPDATE user_data SET privacy = '"+str(p)+"' WHERE user_id = "+str(id))
         conn.commit()
     except db.errors.OperationalError:
         pass
