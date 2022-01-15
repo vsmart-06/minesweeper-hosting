6567337831e77f49f8bb6b4d036862ae42b897fc
vsmart-06 <srivishnuvusirikala@gmail.com>
2022-01-15 21:27:39 +0530

    Shifted back to mysql

--- records.py
+++ records.py
@@ -1,10 +1,36 @@
-import psycopg as db
+import mysql.connector as db
 import os
 
+db_url = os.getenv("JAWSDB_URL")
+db_url_temp = db_url.replace("mysql://", "")
+db_url_temp = db_url_temp.replace("@", ":")
+db_url_temp = db_url_temp.replace("3306/", "")
+u, p, h, d = map(str, db_url_temp.split(":"))
+
+conn = db.connect(
+    host = h,
+    user = u,
+    password = p,
+    database = d
+)
+
+c = conn.cursor()
+try:
+    c.execute('''CREATE TABLE user_data (
+        user_id BIGINT NOT NULL PRIMARY KEY,
+        best_time INT,
+        games_won INT NOT NULL,
+        games_lost INT NOT NULL,
+        total_games INT NOT NULL,
+        win_percent DECIMAL NOT NULL,
+        tot_time INT,
+        avg_time INT,
+        privacy VARCHAR
+        )''')
+except db.errors.ProgrammingError:
+    pass
+
 def stats_update(id, win):
-    db_url = os.getenv("DATABASE_URL")
-    conn = db.connect(db_url)
-    c = conn.cursor()
     try:
         if win == 1:
             c.execute('''INSERT INTO user_data 
@@ -29,20 +55,13 @@ def stats_update(id, win):
             new_percent = (record[2]/new_total)*100
             c.execute("UPDATE user_data SET games_lost = "+str(new_lost)+", total_games = "+str(new_total)+", win_percent = "+str(new_percent)+" WHERE user_id = "+str(id))
         conn.commit()
-    c.close()
-    conn.close()
 
 def score_check(id, time):
-    db_url = os.getenv("DATABASE_URL")
-    conn = db.connect(db_url)
-    c = conn.cursor()
     c.execute("SELECT * FROM user_data WHERE user_id = "+str(id))
     record = c.fetchone()
     if record[1] == None:
         c.execute("UPDATE user_data SET best_time = "+str(time)+", tot_time = "+str(time)+", avg_time = "+str(time)+" WHERE user_id = "+str(id))
         conn.commit()
-        c.close()
-        conn.close()
         return "new high"
     else:
         old_tot_time = record[6]
@@ -52,29 +71,23 @@ def score_check(id, time):
         if time < record[1]:
             c.execute("UPDATE user_data SET best_time = "+str(time)+" WHERE user_id = "+str(id))
             conn.commit()
-            c.close()
-            conn.close()
             return "new record"
     conn.commit()
-    c.close()
-    conn.close()
     return "no change"
     
     
 def global_leaderboard():
-    db_url = os.getenv("DATABASE_URL")
-    conn = db.connect(db_url)
-    c = conn.cursor()
-    c.execute("SELECT user_id, best_time FROM user_data ORDER BY best_time")
-    leaders = c.fetchmany(10)
-    c.close()
-    conn.close()
-    return leaders
+    c.execute('''
+                SELECT user_id, best_time
+                FROM user_data
+                ORDER BY best_time''')
+    leaders = c.fetchall()
+    for record in leaders:
+        if record[1] == None:
+            leaders.remove(record)
+    return leaders[0:10]
 
 def server_leaderboard(members):
-    db_url = os.getenv("DATABASE_URL")
-    conn = db.connect(db_url)
-    c = conn.cursor()
     server_leaders = []
     for x in members:
         try:
@@ -88,30 +101,18 @@ def server_leaderboard(members):
         except ValueError:
             break
     server_leaders.sort(key = lambda a: a[1])
-    c.close()
-    conn.close()
     return server_leaders[0:10]
 
 def profile(id):
-    db_url = os.getenv("DATABASE_URL")
-    conn = db.connect(db_url)
-    c = conn.cursor()
     try:
         c.execute("SELECT * FROM user_data WHERE user_id = "+str(id))
         return c.fetchone()
     except db.errors.OperationalError:
         pass
-    c.close()
-    conn.close()
 
 def privacy_change(id, p):
-    db_url = os.getenv("DATABASE_URL")
-    conn = db.connect(db_url)
-    c = conn.cursor()
     try:
         c.execute("UPDATE user_data SET privacy = '"+str(p)+"' WHERE user_id = "+str(id))
         conn.commit()
     except db.errors.OperationalError:
         pass
-    c.close()
-    conn.close()
